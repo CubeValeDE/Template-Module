f26c50455d8114ea3f4e6670b6eca5d996a01463
Konrad <107440414+Bapty1@users.noreply.github.com>
2023-05-02 23:00:57 +0200

    Add files via upload

---
+++ src/main/java/de/cubevale/modules/template/Template.java
@@ -0,0 +1,21 @@
+package de.cubevale.modules.template;
+
+import de.cubevale.core.api.CorePlugin;
+import de.cubevale.core.api.Module;
+
+public final class Template extends Module {
+
+    public Template(CorePlugin plugin) {
+        super(plugin);
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
