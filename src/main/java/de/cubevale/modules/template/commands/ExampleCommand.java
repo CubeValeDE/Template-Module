b2303e3482ebba47b7b2cf799e29ca04597146ac
Konrad <107440414+Bapty1@users.noreply.github.com>
2024-01-07 01:22:01 +0100

    added examples

---
+++ src/main/java/de/cubevale/modules/template/commands/ExampleCommand.java
@@ -0,0 +1,17 @@
+package me.bapty.test.commands;
+
+import de.cubevale.core.api.command.Command;
+import de.cubevale.core.api.command.CommandOptions;
+import de.cubevale.core.api.user.User;
+
+@CommandOptions(
+        name = "example",
+        permission = "cubevale.command.example",
+        aliases = {"e", "ex"}
+)
+public class ExampleCommand extends Command {
+    @Override
+    public boolean onCommand(User user, String[] args) {
+        return true;
+    }
+}
---
+++ src/main/java/de/cubevale/modules/template/events/JoinEvent.java
@@ -0,0 +1,8 @@
+package me.bapty.test.events;
+
+import de.cubevale.core.api.event.EventListener;
+import de.cubevale.core.api.events.UserJoinEvent;
+
+public class JoinEvent extends EventListener {
+    public void onJoin(UserJoinEvent event) {}
+}
