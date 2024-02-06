feb37b910f4f1f5c821a5039cff9dd4e18b8c85b
Baptist <107440414+Bapty1@users.noreply.github.com>
2024-02-06 16:14:20 +0100

    changed example event

--- src/main/java/de/cubevale/modules/template/events/JoinEvent.java
+++ src/main/java/de/cubevale/modules/template/events/JoinEvent.java
@@ -1,8 +1,13 @@
 package de.cubevale.modules.template.events;
 
+import de.cubevale.core.api.enums.MessageType;
 import de.cubevale.core.api.event.EventListener;
 import de.cubevale.core.api.events.UserJoinEvent;
+import de.cubevale.core.api.user.OnlineUser;
 
 public class JoinEvent extends EventListener {
-    public void onJoin(UserJoinEvent event) {}
+    public void onJoin(UserJoinEvent event) {
+        OnlineUser user = event.getUser();
+        user.sendMessage(MessageType.INFO, "Welcome!");
+    }
 }
